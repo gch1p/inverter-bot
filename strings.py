1815f4be372b44c1ac738e01b59eec5db538c22f
Evgeny Zinoviev <me@ch1p.io>
2021-10-25 00:15:36 +0300

    nice upgrade

--- strings.py
+++ strings.py
@@ -1,9 +1,16 @@
 __strings = {
     'status': 'Status',
     'generation': 'Generation',
-    'gs': 'GS',
-    'ri': 'RI',
-    'errors': 'Errors'
+
+    # flags
+    'flag_buzzer': 'Buzzer',
+    'flag_overload_bypass': 'Overload bypass',
+    'flag_escape_to_default_screen_after_1min_timeout': 'Reset to default LCD page after 1min timeout',
+    'flag_overload_restart': 'Restart on overload',
+    'flag_over_temp_restart': 'Restart on overtemp',
+    'flag_backlight_on': 'LCD backlight',
+    'flag_alarm_on_on_primary_source_interrupt': 'Beep on primary source interrupt',
+    'flag_fault_code_record': 'Fault code recording',
 }
 
 
